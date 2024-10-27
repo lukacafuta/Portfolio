eab723420451cc7e1303e54b3c92f9bcf3d93d71
lukacafuta <luka.cafuta@gmail.com>
2024-10-27 22:48:43 +0100

    toolbox setup

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,14 +4,8 @@ export default function AboutMe() {
         <div className="flex flex-col w-[71rem]">
             <h2 className="text-[1.75rem] font-extrabold">About Me</h2>
             <div className="w-[54.5rem] h-[13.75rem] text-[1.125rem] font-normal">
-                Lorem ipsum dolor sit amet consectetur adipiscing elit litora rutrum, congue eget tempus massa montes
-                vel condimentum. Urna fermentum aptent interdum convallis mauris vel mattis pharetra class, euismod
-                purus tempor quis erat felis nostra ut porttitor at, sapien varius inceptos eros orci sodales potenti
-                morbi. Leo mi tempus nulla.
-                Magna blandit ullamcorper, dictumst eu in habitasse suspendisse faucibus sed, lectus sapien commodo nisi
-                urna. Nascetur dui fermentum malesuada nec urna mauris natoque, magnis per velit senectus facilisi
-                dignissim duis, risus felis cum egestas parturient praesent. Rhoncus mauris class dui sodales eleifend
-                potenti phasellus torquent lobortis, nam lacus.
+                Lorem ipsum dolor sit amet consectetur adipiscing elit litora rutrum, congue eget tempus massa montes vel condimentum. Urna fermentum aptent interdum convallis mauris vel mattis pharetra class, euismod purus tempor quis erat felis nostra ut porttitor at, sapien varius inceptos eros orci sodales potenti morbi. Leo mi tempus nulla. <br/><br/>
+                Magna blandit ullamcorper, dictumst eu in habitasse suspendisse faucibus sed, lectus sapien commodo nisi urna. Nascetur dui fermentum malesuada nec urna mauris natoque, magnis per velit senectus facilisi dignissim duis, risus felis cum egestas parturient praesent. Rhoncus mauris class dui sodales eleifend potenti phasellus torquent lobortis, nam lacus.
             </div>
         </div>
     </section>
