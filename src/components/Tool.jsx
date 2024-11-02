f82415875d49db324f878286b427f83710e793e4
lukacafuta <luka.cafuta@gmail.com>
2024-11-02 21:40:28 +0100

    added tool animation when clicking on icon

--- src/components/Tool.jsx
+++ src/components/Tool.jsx
@@ -1,15 +1,19 @@
-import {useState} from "react";
+export default function Tool({ tool, isFlipped, onClick  }) {
 
-export default function Tool({ tool}) {
-    // const [isFlipped, setIsFlipped] = useState(false);
-
-    // const handleClick = () => {
-    //     setIsFlipped(!isFlipped);
-    // }
 
     return(
-        <div className="flex justify-center items-center w-32 h-16 m-2 hover:scale-110">
-            <img className="w-32 h-16 grayscale hover:filter-none" src={tool.icon} alt="tool logo"/>
+        <div className={`flex justify-center items-center w-32 h-16 m-2 hover:scale-110 cursor-pointer ${
+            isFlipped ? "animate-flip" : ""
+        }`}
+        onClick={onClick}
+        >
+            {isFlipped ? (
+                <div className="flex justify-center items-center w-32 h-16">
+                    <span className="text-center">{tool.name}</span>
+                </div>
+            ) : (
+                <img className="w-32 h-16 grayscale hover:filter-none" src={tool.icon} alt="tool logo"/>
+            )}
         </div>
-    )
+    );
 }
--- src/components/Toolbox.jsx
+++ src/components/Toolbox.jsx
@@ -1,8 +1,15 @@
 import tools from '/data/tools.json';
 import Tool from "./Tool.jsx";
+import {useState} from "react";
 
 
 export default function Toolbox() {
+
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleClick = () => {
+        setIsFlipped(!isFlipped);
+    }
 
     return (
     <section className="flex flex-row justify-center px-32">
@@ -10,7 +17,7 @@ export default function Toolbox() {
             <h2 className="text-[1.75rem] font-extrabold">My Tools</h2>
             <div className="grid grid-cols-5 w-full gap-12 w-[54.5rem] text-[1.125rem] font-normal">
                 {tools.map((tool)=>(
-                    <Tool key={tool.id} tool={tool}/>
+                    <Tool key={tool.id} tool={tool} isFlipped={isFlipped} onClick={handleClick} />
                 ))}
             </div>
         </div>
