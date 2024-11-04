892ecc85add872a38b32e331489d346da7fae0b8
lukacafuta <luka.cafuta@gmail.com>
2024-11-04 22:35:46 +0100

    dividing footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
 export default function Footer() {
   return (
-    <footer>
-      <p>Footer for my website</p>
-    </footer>
+      <footer className="flex flex-row justify-between w-full pb-14">
+          <p>Email: luka.cafuta@gmail.com</p>
+          <p>Location: Zurich, Switzerland</p>
+          <div>Facebook</div>
+          <div>Instagram</div>
+          <div>Whatsapp</div>
+          <div>X</div>
+      </footer>
   );
 }
