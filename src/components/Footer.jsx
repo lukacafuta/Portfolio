95c54a03bec155265463b1979f037f51a753c1b6
lukacafuta <luka.cafuta@gmail.com>
2024-11-05 21:08:21 +0100

    deleted the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,6 @@
 export default function Footer() {
   return (
-      <footer className="flex flex-row justify-between w-full pb-14">
-          <p>Email: luka.cafuta@gmail.com</p>
-          <p>Location: Zurich, Switzerland</p>
-          <div>Facebook</div>
-          <div>Instagram</div>
-          <div>Whatsapp</div>
-          <div>X</div>
+      <footer className="pb-14">
       </footer>
   );
 }
