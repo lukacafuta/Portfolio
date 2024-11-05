cea5e83252e4311717404c4e0fdc8af3f2f3f820
lukacafuta <luka.cafuta@gmail.com>
2024-11-05 22:10:58 +0100

    removed animation, added name while hovering, switched grid to flexbox

--- src/components/Tool.jsx
+++ src/components/Tool.jsx
@@ -1,19 +1,9 @@
-export default function Tool({ tool, isFlipped, onClick  }) {
-
+export default function Tool({ tool }) {
 
     return(
-        <div className={`flex justify-center items-center w-32 h-16 m-2 hover:scale-110 cursor-pointer ${
-            isFlipped ? "animate-flip" : ""
-        }`}
-        onClick={onClick}
-        >
-            {isFlipped ? (
-                <div className="flex justify-center items-center w-32 h-16">
-                    <span className="text-center">{tool.name}</span>
-                </div>
-            ) : (
-                <img className="w-32 h-16 grayscale hover:filter-none" src={tool.icon} alt="tool logo"/>
-            )}
+        <div className="relative flex flex-col justify-center items-center w-32 h-16 m-2 hover:scale-110 cursor-pointer group">
+            <img className="w-32 h-16 grayscale hover:filter-none" src={tool.icon} alt="tool logo"/>
+            <span className="mt-2 text-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">{tool.name}</span>
         </div>
     );
 }
--- src/components/Toolbox.jsx
+++ src/components/Toolbox.jsx
@@ -1,23 +1,20 @@
 import tools from '/data/tools.json';
 import Tool from "./Tool.jsx";
-import {useState} from "react";
+
 
 
 export default function Toolbox() {
 
-    const [isFlipped, setIsFlipped] = useState(false);
-
-    const handleClick = () => {
-        setIsFlipped(!isFlipped);
-    }
 
     return (
     <section className="flex flex-row justify-center px-32">
         <div className="flex flex-col w-[71rem]">
-            <h2 className="text-[1.75rem] font-extrabold">My Tools</h2>
-            <div className="grid grid-cols-5 w-full gap-12 w-[54.5rem] text-[1.125rem] font-normal">
+            <h2 className="text-[1.75rem] font-extrabold pb-6">My Tools</h2>
+            <div className="flex flex-wrap w-full justify-between text-[1.125rem] font-normal">
                 {tools.map((tool)=>(
-                    <Tool key={tool.id} tool={tool} isFlipped={isFlipped} onClick={handleClick} />
+                    <div key={tool.id} className="flex-grow-0 flex-shrink-0 basis-[18%] mb-12">
+                    <Tool tool={tool} />
+                    </div>
                 ))}
             </div>
         </div>
