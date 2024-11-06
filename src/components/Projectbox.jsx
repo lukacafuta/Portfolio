821a747a6150917c6e1012f6c9522964d77d073a
lukacafuta <luka.cafuta@gmail.com>
2024-11-06 14:00:38 +0100

    main Projectbox setup + content

--- src/components/Projectbox.jsx
+++ src/components/Projectbox.jsx
@@ -4,11 +4,23 @@ export default function Projectbox() {
         <section className="flex flex-row justify-center">
             <div className="flex flex-col w-full">
                 <h2 className="text-[1.75rem] font-extrabold pb-6">Work</h2>
-                <div className="w-full flex flex-row justify-between gap-4">
-                    <div className="w-64 h-52 rounded-lg bg-custom-linkedin-burgundy shadow-md">Project 1</div>
-                    <div className="w-64 h-52 rounded-lg bg-custom-linkedin-burgundy shadow-md">Project 2</div>
-                    <div className="w-64 h-52 rounded-lg bg-custom-linkedin-burgundy shadow-md">Project 3</div>
-                    <div className="w-64 h-52 rounded-lg bg-custom-linkedin-burgundy shadow-md">Project 4</div>
+                <div className="w-full flex flex-row gap-12">
+                    <div className="w-[50%] h-[28rem] rounded-2xl bg-custom-light-brown">
+                        <div className="flex flex-col gap">
+                            <div>ID</div>
+                            <h2>TITLE</h2>
+                            <p>DESCRIPTION</p>
+                            <li>TECHNOLOGIES</li>
+                        </div>
+                    </div>
+                    <div className="w-[50%] h-[28rem] rounded-2xl bg-custom-light-brown shadow-xl">
+                        <div>Project</div>
+                        <div className="flex flex-row gap-2">
+                            <button className="border-2 cursor-pointer">next</button>
+                            <button className="border-2 cursor-pointer">previous</button>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </section>
