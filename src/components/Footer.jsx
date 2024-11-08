ebb3b4ef775a0fe12fd93ca5f6ff81d67aa41980
lukacafuta <luka.cafuta@gmail.com>
2024-11-08 16:51:59 +0100

    added chopin font and styled footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,17 @@
 export default function Footer() {
   return (
       <footer className="pb-14">
+          <div className="border-t opacity-10 mb-20"></div>
+          <span className="flex justify-center font-chopin text-3xl opacity-50 font-thin">
+              "I walk around like everything's fine, but deep down, inside my shoe, my sock is sliding off."
+          </span>
+          <span className="flex flex-row justify-end font-chopin text-2xl opacity-50 font-thin mt-12 mr-32">
+          — Unknown
+        </span>
+          <div className="flex flex-row justify-center mt-16">
+              <img src="/copyright-darkmode.svg" className="opacity-30"/>
+              <span className="opacity-30">2024 Luka Cafuta</span>
+          </div>
       </footer>
   );
 }
