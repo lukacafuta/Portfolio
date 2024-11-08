d4337a1554f71dbf8c85a5d1c3951ea75babec27
lukacafuta <luka.cafuta@gmail.com>
2024-11-08 21:32:48 +0100

    changed the colors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,17 @@
 export default function Hero() {
   return (
-    <section className="flex flex-row justify-between mb-">
+    <section className="flex flex-row justify-between -mb-4">
         <div className="flex flex-col justify-center">
-            <h1 className="text-6xl font-extrabold pb-4">
-                Sali, I'm <span className="block pt-4">Luka Cafuta</span>
+            <h1 className="text-5xl font-extrabold pb-4">
+                Sali, I'm Luka Cafuta
             </h1>
-            <h2 className="text-3xl font-semibold pb-12">Full Stack Developer from Switzerland.</h2>
+            <h2 className="text-2xl font-semibold pb-12">Full Stack Developer from Switzerland.</h2>
             <div className="flex flex-row gap-4">
                 <a
                     href="https://www.linkedin.com/in/luka-cafuta"
                     target="_blank"
                     rel="noopener noreferrer" // This is a security measure to prevent malicious websites from accessing your data
-                    className="flex flex-row items-center justify-center bg-custom-linkedin-burgundy w-32 h-10 rounded-lg gap-2 text-md cursor-pointer hover:scale-110"
+                    className="flex flex-row items-center justify-center bg-custom-milk-chocolate-brown w-32 h-10 rounded-lg gap-2 text-md cursor-pointer hover:scale-110"
                 >
                     <img src="/linkedin-logo.svg" alt="LinkedIn button"/>
                     <div className="font-semibold font-poppins">
@@ -22,7 +22,7 @@ export default function Hero() {
                     href="https://github.com/lukacafuta"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="flex flex-row items-center justify-center bg-custom-github-dark-green w-32 h-10 rounded-lg gap-2 text-md cursor-pointer hover:scale-110"
+                    className="flex flex-row items-center justify-center w-32 h-10 border border-custom-milk-chocolate-brown rounded-lg gap-2 text-md cursor-pointer hover:scale-110"
                 >
                     <img src="/github-logo.svg" alt="GitHub button"/>
                     <div className="font-semibold font-poppins">
@@ -32,7 +32,7 @@ export default function Hero() {
             </div>
         </div>
         <div>
-        <img src="/profile-photo.svg" alt="Profile photo"/>
+        <img src="/profile-photo-darkmode.svg" alt="Profile photo"/>
         </div>
     </section>
   );
--- src/components/ThemeToggleButton.jsx
+++ src/components/ThemeToggleButton.jsx
@@ -11,8 +11,10 @@ export default function ThemeToggleButton() {
         <div className="flex items-center space-x-2">
             <img src="/lightmode-icon.svg" alt="Light mode icon" className="w-6 h-6" />
 
-            <div className="flex items-center cursor-pointer bg-custom-toggle-slider-grey w-14 h-8  rounded-full p-1" onClick={toggleTheme}>
-                <div className={`w-6 h-6 rounded-full shadow-md transform transition-transform ${isLightMode ? 'translate-x-0 bg-custom-linkedin-burgundy' : 'translate-x-6 bg-custom-github-dark-green'}`}></div>
+            <div className="flex items-center cursor-pointer bg-custom-light-chocolate-brown w-14 h-8 rounded-full p-1"
+                 onClick={toggleTheme}
+            >
+                <div className={`w-6 h-6 rounded-full shadow-md transform transition-transform bg-custom-milk-chocolate-brown ${isLightMode ? 'translate-x-0' : 'translate-x-6'}`}></div>
             </div>
 
             <img src="/darkmode-icon.svg" alt="Dark mode icon"  className="w-6 h-6 ml-auto"/>
