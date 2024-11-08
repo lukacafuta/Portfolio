1b2b786ab11f1393806e611eb360dfdfc91975c9
lukacafuta <luka.cafuta@gmail.com>
2024-11-08 10:57:35 +0100

    removed the tool.name and centered the icons

--- src/components/Tool.jsx
+++ src/components/Tool.jsx
@@ -1,9 +1,8 @@
 export default function Tool({ tool }) {
 
     return(
-        <div className="relative flex flex-col justify-center items-center w-32 h-16 m-2 hover:scale-110 cursor-pointer group">
-            <img className="w-32 h-16 grayscale hover:filter-none" src={tool.icon} alt="tool logo"/>
-            <span className="mt-2 text-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 font-poppins">{tool.name}</span>
+        <div className="flex flex-col justify-center items-center w-32 h-16 m-2 hover:scale-110 cursor-pointer">
+            <img className="w-full h-full object-contain grayscale hover:filter-none" src={tool.icon} alt="tool logo"/>
         </div>
     );
 }
