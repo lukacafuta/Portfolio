0159d28cdd99ee1c6fed1a100abefea0e241211e
lukacafuta <luka.cafuta@gmail.com>
2024-11-09 01:00:13 +0100

    added dark mode/light mode functionality

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,22 @@
-export default function Footer() {
+export default function Footer({ theme }) {
   return (
       <footer>
-          <div className="border-t opacity-10 mb-20 mx-8"></div>
+          <div className="border-t opacity-10 mb-20 mx-8 border-custom-dark-chocolate-brown dark:border-custom-light-chocolate-brown"></div>
 
           <div className="flex flex-col mt-24">
-              <div className="flex text-4xl max-w-md opacity-50 text-custom-light-chocolate-brown font-bold ml-40 self-center">
+              <div className="flex text-4xl max-w-md opacity-50 font-bold ml-40 self-center text-custom-dark-chocolate-brown dark:text-custom-light-chocolate-brown">
                   "I walk around like everything's fine, but deep down, inside my shoe, my sock is sliding off."
               </div>
-              <div
-                  className="flex flex-row justify-end text-2xl opacity-50 text-custom-light-chocolate-brown font-thin mt-12 mr-32">
+              <div className="flex flex-row justify-end text-2xl opacity-50 font-thin mt-12 mr-32 text-custom-dark-chocolate-brown dark:text-custom-light-chocolate-brown">
                   — Unknown
               </div>
           </div>
 
           <div className="flex flex-row justify-center items-center text-sm mt-24">
-              <img src="/copyright-darkmode.svg" className="opacity-30"/>
-              <span className="opacity-30">2024 Luka Cafuta</span>
+              <img
+                  src={theme === 'light' ? "/copyright-dark-brown.svg" : "/copyright-light-brown.svg"}
+                  className="opacity-30"/>
+              <span className="opacity-30 text-custom-dark-chocolate-brown dark:text-custom-light-chocolate-brown">2024 Luka Cafuta</span>
           </div>
       </footer>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
-export default function Hero() {
+export default function Hero({ theme }) {
   return (
     <section className="flex flex-row justify-between -mb-4">
         <div className="flex flex-col justify-center">
-            <h1 className="text-5xl font-extrabold pb-4">
+            <h1 className="text-5xl font-extrabold pb-4 text-custom-dark-chocolate-brown dark:text-custom-light-chocolate-brown">
                 Sali, I'm Luka Cafuta
             </h1>
-            <h2 className="text-2xl font-semibold pb-12">Full Stack Developer from Switzerland.</h2>
+            <h2 className="text-2xl font-semibold pb-12 text-custom-dark-chocolate-brown dark:text-custom-light-chocolate-brown">Full Stack Developer from Switzerland.</h2>
             <div className="flex flex-row gap-4">
                 <a
                     href="https://www.linkedin.com/in/luka-cafuta"
@@ -13,8 +13,8 @@ export default function Hero() {
                     rel="noopener noreferrer" // This is a security measure to prevent malicious websites from accessing your data
                     className="flex flex-row items-center justify-center bg-custom-milk-chocolate-brown w-32 h-10 rounded-lg gap-2 text-md cursor-pointer hover:scale-110"
                 >
-                    <img src="/linkedin-logo.svg" alt="LinkedIn button"/>
-                    <div className="font-semibold font-poppins">
+                    <img src="/linkedin-logo-light-brown.svg" alt="LinkedIn button"/>
+                    <div className="font-semibold text-custom-light-chocolate-brown">
                         LinkedIn
                     </div>
                 </a>
@@ -24,15 +24,16 @@ export default function Hero() {
                     rel="noopener noreferrer"
                     className="flex flex-row items-center justify-center w-32 h-10 border border-custom-milk-chocolate-brown rounded-lg gap-2 text-md cursor-pointer hover:scale-110"
                 >
-                    <img src="/github-logo.svg" alt="GitHub button"/>
-                    <div className="font-semibold font-poppins">
+                    <img src={theme === 'light' ? "/github-logo-lightmode.svg" : "/github-logo-light-brown.svg"} alt="GitHub button"/>
+
+                    <div className="font-semibold text-custom-dark-chocolate-brown dark:text-custom-light-chocolate-brown">
                         GitHub
                     </div>
                 </a>
             </div>
         </div>
         <div>
-        <img src="/profile-photo-darkmode.svg" alt="Profile photo"/>
+        <img src={theme === 'light' ? "/profile-photo-lightmode.svg" : "/profile-photo-darkmode.svg"} alt="Profile photo"/>
         </div>
     </section>
   );
